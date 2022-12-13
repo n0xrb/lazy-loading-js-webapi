6b66e0b81922e71296a6f03136cbaa61c27cfdaa
Alex V <alex.v@nxrub.pw>
2022-12-12 21:59:11 -0300

    main

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { imageRegister } from "./lazy";
 
-console.log('Happy hacking :)')
+const createImageNode = () => {
+	const randomNumber = Math.floor(Math.random() * 100 + 1);
+	const containerNode = document.createElement("div");
+	containerNode.className = "p-4";
+
+	const image = document.createElement("img");
+	image.className = "mx-auto";
+	image.width = "320";
+	image.dataset.src = `https://randomfox.ca/images/${randomNumber}.jpg`;
+
+	containerNode.append(image);
+	return containerNode;
+};
+
+const containerImage = document.getElementById("images");
+
+document.getElementById("addNewImage").addEventListener("click", () => {
+	const nuevaImagen = createImageNode();
+	imageRegister(nuevaImagen);
+	containerImage.append(nuevaImagen);
+});
+
+document.getElementById("clearImageContainer").addEventListener("click", () => {
+	containerImage.innerHTML = "";
+});
